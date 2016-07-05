2bf39c23ab36b63f2f40fa0027cb84a1b7085a99
unknown <yilai.wl@hz.ali.com>
2016-07-05 22:38:27 +0800

    a-new-start

---
+++ init.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tango-dark))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(load "d:/sync/emacs/emacs.d/load.el")
---
+++ load.el
@@ -0,0 +1,33 @@
+;; font
+(if (string= system-name "lab")
+    (progn
+      (set-default-font "monaco-10")
+      (set-fontset-font "fontset-default" 'gb18030 "kaiti-12"))
+  (progn
+    (set-default-font "monaco-11")
+    (set-fontset-font "fontset-default" 'gb18030 "kaiti-13")))
+
+;; frame
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(setq default-frame-alist '((height . 30)))
+(setq frame-title-format "")
+
+;; language
+(set-buffer-file-coding-system 'chinese-gbk)
+(set-keyboard-coding-system 'chinese-gbk)
+(set-language-environment 'chinese-gbk)
+
+;; startup
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+(setq make-backup-files nil)
+(setq ring-bell-function 'ignore)
+(setq save-abbrevs nil)
+
+;; ~load
+(setq default-directory "d:/sync/")
+(add-to-list 'load-path "d:/sync/emacs/emacs.d/")
+(load "hook.el")
+(load "command.el")
+(load "kbd.el")
